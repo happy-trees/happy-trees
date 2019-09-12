aa3bace29abbad95eb97ae381d1968eb3d0d9f79
emily <mle4d@me.com>
2019-09-12 15:50:59 -0700

    some for-now styling with status bar

--- src/components/gameInput/StatusBar.js
+++ src/components/gameInput/StatusBar.js
@@ -2,20 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './statusbar.css';
 
-function StatusBar({ guess, handleChange }) {
+
+function StatusBar({ nickName, handleChange }) {
   return (
-    <div className={styles.StatusBar} >
+    <div className={styles.StatusBar}>
       <div className={styles.round}>1 of 5</div>
       <div className={styles.timerContainer}>timer</div>
-      <div className={styles.currentArtist}>{guess}Danny</div>
-      <label className={styles.brushTitle}htmlFor="brush">Brush Color</label>
-      <input type="color" id="brush" name="brush" value="#FF914D" onChange={handleChange} />
+      <div className={styles.currentArtist}>{nickName}Drawer: Danny</div>
+      <div className={styles.colorPicker}>
+        <label className={styles.brushTitle}htmlFor="brush">Brush Color</label>
+        <input className={styles.brushBar} type="color" id="brush" name="brush" value="#FF914D" onChange={handleChange} />
+      </div>
     </div>
   );
 
 }
 StatusBar.propTypes = {
-  guess: PropTypes.string,
+  nickName: PropTypes.string,
   handleChange: PropTypes.func
 };
 
