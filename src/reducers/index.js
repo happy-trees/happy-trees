c52bd0c73770b9ffb625f9919c73591b9abfc259
Jackmc <jackmc1122@gmail.com>
2019-09-13 09:43:33 -0700

    Add redux stuff for auth

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import drawing from './drawingReducer';
 import socket from './socketReducer';
+import user from './authReducer';
 
 export default combineReducers({
   drawing,
-  socket
+  socket,
+  user
 });
