8b707c5c5a113779a04806220136d81765169954
DannySuarez <suarezd10@gmail.com>
2019-09-13 11:07:30 -0700

    Add GameInput coontainer W/styles

--- src/components/gameInput/GameInput.js
+++ src/components/gameInput/GameInput.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './gameinput.css';
 
-function GameInput({ guess, handleSubmit, handleChange  }) {
+function GameInput({ guesses, guess, handleSubmit, handleChange  }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="guess" value={guess} onChange={handleChange} />
-    </form>
+    
+    <footer>
+      <section>
+        <aside>
+          <p>You have {guesses} 3 guesses left</p>
+        </aside>
+      </section>
+
+      <section>
+        <aside className="rounds">
+          <p>Round 1{} of 5</p>
+        </aside>
+      </section>
+    
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input className={styles.input} type="text" name="guess" value={guess} onChange={handleChange} />
+      </form>
+    </footer>
+    
   );
 
 }
 
 GameInput.propTypes = {
+  guesses: PropTypes.string,
   guess: PropTypes.string,
   handleSubmit: PropTypes.func,
   handleChange: PropTypes.func
