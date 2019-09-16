5df9b4b75b44ffec71d7e8b9059daf2132d6cb65
Jackmc <jackmc1122@gmail.com>
2019-09-16 09:30:53 -0700

    Remove double link

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PlayButton from './PlayButton';
-import { Link } from 'react-router-dom';
 
 function Home({ nickname, avatar }) {
   return (
     <>
-    <p>{nickname}</p>
-    <img src={avatar} />
-    <Link to="game">
+      <p>{nickname}</p>
+      <img src={avatar} />
       <PlayButton />
-    </Link>
     </>
   );
 }
