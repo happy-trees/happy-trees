503d1c1df7a555fa7869728caf5e1d215c05234b
emily <mle4d@me.com>
2019-09-16 09:39:28 -0700

    take out link

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PlayButton from './PlayButton';
-import { Link } from 'react-router-dom';
+
 
 function Home({ nickname, avatar }) {
   return (
     <>
     <p>{nickname}</p>
     <img src={avatar} />
-    <Link to="game">
       <PlayButton />
-    </Link>
     </>
   );
 }
