8b91c87789d2fb7a17b49b059e595916e9cf7df7
Jackmc <jackmc1122@gmail.com>
2019-09-17 13:55:55 -0700

    Fix round numbers bug

--- src/selectors/socketSelectors.js
+++ src/selectors/socketSelectors.js
@@ -4,3 +4,4 @@ export const getGameId = state => getSocketState(state).gameId;
 export const getRoundId = state => getSocketState(state).roundId;
 export const getIsPlaying = state => getSocketState(state).isPlaying;
 export const getGuesses = state => getSocketState(state).guesses;
+export const getRoundNumber = state => getSocketState(state).roundNumber;
