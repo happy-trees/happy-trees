24e0ef387f5b12017368ae800d4c190a62a2d7f8
codingclueless <codingclueless@gmail.com>
2019-09-19 10:27:41 -0700

    gamepage style

--- src/components/gameInput/StatusBar.js
+++ src/components/gameInput/StatusBar.js
@@ -5,13 +5,13 @@ import styles from './statusbar.css';
 
 function StatusBar({ handleChange, time, currentDrawer, roundNumber, color }) {
   return (
-    <>
+     <>
     <div className={styles.StatusBar}>
-      <div className={styles.round}>Round: {roundNumber}</div>
-      <div className={styles.timerContainer}>timer: {time}</div>
-      <div className={styles.currentArtist}>Drawer: {currentDrawer}</div>
+      <div className={styles.round}>Round<div className={styles.roundColor}>{roundNumber}</div></div>
+      <div className={styles.timerContainer}>timer<div className={styles.timeColor}>{time}</div></div>
+      <div className={styles.currentArtist}>Drawer<div className={styles.drawerColor}>{currentDrawer}</div></div>
       <div className={styles.colorPicker}>
-        <label className={styles.brushTitle}htmlFor="color">Brush Color</label>
+        <label className={styles.brushTitle}htmlFor="color">Color</label>
         <input className={styles.brushBar} type="color" id="color" name="color" value={color} onChange={handleChange}/>
       </div>
     </div>
