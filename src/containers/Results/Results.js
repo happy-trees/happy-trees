0e943e2c2be2e6476d978e2b231f51e3b3a5e888
Jackmc <jackmc1122@gmail.com>
2019-09-19 10:20:21 -0700

    Display scores

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -5,10 +5,29 @@ import { Link } from 'react-router-dom';
 import { getScores } from '../../selectors/socketSelectors';
 
 const Results = ({ scores }) => {
-  console.log(scores);
+  const result = () => {
+    if(scores[0].score !== scores[1].score) {
+      return (
+        <section>
+          <h2>FIRST PLACE</h2>
+          <h3>{scores[0].user.nickname} - {scores[0].score} pts</h3>
+          <h2>SECOND PLACE</h2>
+          <h3>{scores[1].user.nickname} - {scores[1].score} pts</h3>
+        </section>
+      );
+    } else {
+      return (
+        <section>
+          <h3>It was a tie!</h3>
+          <h3>Both players had {scores[0].score} points!</h3>
+        </section>
+      );
+    }
+  };
   return (
     <section>
       <h1>Results</h1>
+      {result()}
       <Link to="/">
         <button>
           Home
