3d971eb2280d749456f3ed596de3fb4633e37c9e
emily <mle4d@me.com>
2019-09-19 16:23:10 -0700

    result styling thus far

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -3,30 +3,31 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getScores } from '../../selectors/socketSelectors';
+import styles from './results.css';
 
 const Results = ({ scores }) => {
   const result = () => {
     if(scores[0].score !== scores[1].score) {
       return (
-        <section>
-          <h2>FIRST PLACE</h2>
+        <section className={styles.resultsSection}>
+          <h2 className={styles.resultTitle}>FIRST PLACE</h2>
           <h3>{scores[0].user.nickname} - {scores[0].score} pts</h3>
-          <h2>SECOND PLACE</h2>
+          <h2 className={styles.resultTitle}>SECOND PLACE</h2>
           <h3>{scores[1].user.nickname} - {scores[1].score} pts</h3>
         </section>
       );
     } else {
       return (
-        <section>
-          <h3>It was a tie!</h3>
+        <section className={styles.resultsSection}>
+          <h2 className={styles.resultTitle}>It was a tie!</h2>
           <h3>Both players had {scores[0].score} points!</h3>
         </section>
       );
     }
   };
   return (
-    <section>
-      <h1>Results</h1>
+    <section className={styles.resultsBox}>
+      <h1 className={styles.resultHeader}>Results</h1>
       {result()}
       <Link to="/">
         <button>
